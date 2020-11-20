59b0ef5f3c75ef85bc7a9c6f5f998a77a0b44c7c
kp1129 <katya.p.1129@gmail.com>
2020-11-20 13:05:38 -0700

    add comments

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,29 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Editor from "./Editor";
 import { StateContext } from "./contexts/StateContext";
 
 function App() {
+  // controls whether or not dark mode is enabled
   const [darkMode, setDarkMode] = useState(false);
+  // controls whether or not the modal is displayed
   const [modal, setModal] = useState(true);
+  // required for reactstrap dropdown menu to work -
+  // controls if dropdown is collapsed or displayed
   const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  // The next two slices of state are updated by the Editor component
+  // string representation of the words written
   const [text, setText] = useState("");
+  // number of words written
   const [wordsWritten, setWordsWritten] = useState(0);
-  const [wordsPerLight, setWordsPerLight] = useState(167);
+
+  // number of lights to turn on based on the challenge parameters and words written
   const [lightsToTurnOn, setLightsToTurnOn] = useState(0);
+
+  // how many words it takes to power one light (this is set by user)
+  const [wordsPerLight, setWordsPerLight] = useState(167);
+  
 
   return (
     <StateContext.Provider
